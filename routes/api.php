a4a7dbe799ebee8612136d9e21d3a52e5b182b1e
jjeeunit <jjeeunit@gmail.com>
2024-02-28 01:13:36 +0900

    npm

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('/users', function () {
+//     return 'hi';
+// });
+
+// Route::get('/users/{name?}', function ($name = null) {
+//     return 'hi'. $name;
+// });
